373ad62824ef33dd949eeecf47609e1b0c32b092
Simon Tokumine <simon@japancentre.com>
2009-12-02 09:26:07 +0000

    updated fonts

--- app/models/postmaster.rb
+++ app/models/postmaster.rb
@@ -1,6 +1,6 @@
 class Postmaster < ActionMailer::Base
   def registration(user)
-  recipients ["One Oceans <OneOcean@unep-wcmc.org>", "simon.tokumine@unep-wcmc.org"]
+  recipients "One Oceans <OneOcean@unep-wcmc.org>"
   from       "One Oceans Data Portal"
   bcc        ["Simon <si@tinypla.net>"]
   subject    "New Registration"

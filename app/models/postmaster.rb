fbe9cd6b873fa6cf1ffa233a89f9c8091968ac42
James Cox <th3james@gmail.com>
2010-07-30 15:51:43 +0100

    Changed names over to MADSPA

--- app/models/postmaster.rb
+++ app/models/postmaster.rb
@@ -1,7 +1,7 @@
 class Postmaster < ActionMailer::Base
   def registration(user)
   recipients ["One Oceans <OneOcean@unep-wcmc.org>", "Simon.Blyth@unep-wcmc.org"]
-  from       "One Oceans Data Portal"
+  from       "Ocean Data Viewer"
   bcc        ["Simon <si@tinypla.net>"]
   subject    "New Registration"
   sent_on    Time.now

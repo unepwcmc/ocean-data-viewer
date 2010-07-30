bd4bc5d0b7daa2068237ab0359518d1cb34cb51b
James Cox <th3james@gmail.com>
2010-07-30 14:23:24 +0100

    Removed validation of shp_download field

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -3,7 +3,7 @@ class Dataset < ActiveRecord::Base
   has_many :customers
   
   validates_presence_of :title, :short_desc, :long_desc, :metadata_description, :licence
-  validates_format_of :shp_download, :wms, :wfs, :meta_download, :with => /^((http|https):\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/, :allow_blank => true
+  validates_format_of :wms, :wfs, :meta_download, :with => /^((http|https):\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/, :allow_blank => true
   
   has_attached_file :html,
                      :storage => :s3,

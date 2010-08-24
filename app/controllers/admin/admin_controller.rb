eaaca278208608e00de01e476b2b86683f53d9bd
James Cox <th3james@gmail.com>
2010-08-24 15:54:20 +0100

    Recommitting admin controller due to weirdness

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -3,10 +3,10 @@ class Admin::AdminController < ApplicationController
   
   before_filter :verify_access
   
-   def verify_access
-     authenticate_or_request_with_http_basic("LOGIN") do |username, password|
-       username == "oneocean" && password == "conserveworld"
-     end
-   end
+  def verify_access
+    authenticate_or_request_with_http_basic("LOGIN") do |username, password|
+      username == "oneocean" && password == "conserveworld"
+    end
+  end
   
 end

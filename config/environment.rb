01ab61058c867906fa047ee9eb3bc5afdae9ee89
James Cox <th3james@gmail.com>
2010-08-25 14:40:01 +0100

    Added backup gem, changed dataset index to show WMS name

--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,8 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "RedCloth"
   config.gem "paperclip", :ver => "2.3.1.1"
+  config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "backup"
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]

3884a240b9ef40fed27c63a7a1e81c99279e100c
James Cox <th3james@gmail.com>
2010-08-26 17:11:10 +0100

    Strip WMS values before saving

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -6,6 +6,9 @@ class Dataset < ActiveRecord::Base
   
   validates_presence_of :title, :short_desc, :long_desc, :metadata_description, :licence
   validates_format_of :wms_server, :wfs, :meta_download, :with => /^((http|https):\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/, :allow_blank => true
+
+  before_save :strip_urls
+
   
   has_attached_file :html,
                      :storage => :s3,
@@ -20,4 +23,10 @@ class Dataset < ActiveRecord::Base
                      :bucket => "marine-portal",
                      :path => "resources/:class/:attachment/:id/:style_:basename.:extension",
                      :url => ":s3_domain_url"
+
+  private
+
+  def strip_urls
+    [wms_name, wms_server].each {|s| s.strip!}
+  end
 end

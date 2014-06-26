2012a83a01cee0add92eef2279aa82519670c24a
Julia Jacobs <julia@llp.pl>
2014-06-26 11:36:51 +0200

    Refactor dataset list using angular

--- app/assets/javascripts/dataset-list.js
+++ app/assets/javascripts/dataset-list.js
@@ -3,30 +3,4 @@ $(function () {
     autoReinitialise: true,
     verticalGutter: 0
   });
-
-  $('div#scrollable_databases ul li').click(function(ev){
-    if ($(this).hasClass('active')){
-      $(this).removeClass('active');
-    }
-    else {
-      $(this).addClass('active');
-    }
-  });
-
-  $('.arrow-more').click(function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    $(this).closest('li').find('.additional-info').slideDown(200);
-    $(this).closest('li').find('.arrow-more').hide();
-    $(this).closest('li').find('.arrow-less').show();
-  });
-
-  $('.arrow-less').click(function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    $(this).closest('li').find('.additional-info').slideUp(200);
-    $(this).closest('li').find('.arrow-less').hide();
-    $(this).closest('li').find('.arrow-more').removeAttr('style');
-    $(this).closest('li').find('.arrow-less').removeAttr('style');
-  });
 });
--- app/presenters/dataset_presenter.rb
+++ app/presenters/dataset_presenter.rb
@@ -1,36 +1,4 @@
 class DatasetPresenter < SimpleDelegator
-  def creation_date
-    "<span>#{dataset.creation_date.strftime("%Y")}</span>".html_safe if dataset.creation_date
-  end
-
-  def data_categories
-    if dataset.data_categories.present?
-      "<span class='category prepend-pipe'>#{dataset.data_categories.map(&:name).join(', ')}</span><br/>".html_safe
-    end
-  end
-
-  def format
-    if %w(polygon point raster).include?(dataset.format)
-      "<span class='prepend-symbol prepend-pipe #{dataset.format}'>#{dataset.format.to_s.capitalize}</span><br/>".html_safe
-    elsif dataset.format == 'point & polygon'
-      "<span class='prepend-symbol prepend-pipe point'>Point + </span><span class='prepend-symbol polygon'>Polygon</span><br/>".html_safe
-    end
-  end
-
-  def time_range
-    if dataset.time_range_start && dataset.time_range_end
-      "<span>Time range: #{dataset.time_range_start} - #{dataset.time_range_end}</span>".html_safe
-    end
-  end
-
-  def observation_type
-    "<span class='prepend-pipe'>#{dataset.observation_type.capitalize}</span>".html_safe if dataset.observation_type
-  end
-
-  def data_provider
-    "<span class='new-line'>Data provider: #{dataset.data_provider}</span>".html_safe if dataset.data_provider.present?
-  end
-
   def dataset
     __getobj__
   end

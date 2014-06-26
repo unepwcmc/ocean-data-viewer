71f68be5e5722cfbdfead2fa342da250a33b8624
Rafael Caricio <rafael.jacinto@gmail.com>
2014-06-26 17:13:56 +0200

    Basic search with fields without design

--- app/assets/javascripts/angular/controllers/map_controller.js
+++ app/assets/javascripts/angular/controllers/map_controller.js
@@ -1,10 +1,57 @@
 var module = angular.module("MarineApp", ['directives.map', 'resources.mapSearch']);
 
+// needs transform the :key's to an array keeping only then ones which the :value is true
+function selectedValues(obj) {
+  var keysWithTrueValue = [];
+  for (var key in obj) {
+    if (obj[key]) {
+      keysWithTrueValue.push(key);
+    }
+  }
+  return keysWithTrueValue;
+}
+
 module.controller('SearchCtrl', ['$scope', 'MapSearch', function($scope, MapSearch) {
 
   MapSearch.query({sort_by: 'creation_date'}).then(function(result) {
     $scope.$broadcast('searchResults', result);
   });
+
+  $scope.sortByOptions = [
+    {text: 'Creation date', value: 'creation_date'},
+    {text: 'Title', value: 'title'}
+  ];
+
+  $scope.filters = {
+    sort_by: 'creation_date',
+    data_categories: {},
+    formats: {},
+    observation_types: {},
+    geographical_ranges: {}
+  };
+
+  // filters.data_categories = {1: true, 2: false}
+
+  $scope.doSearch = function () {
+    var queryOptions = {sort_by: $scope.filters.sort_by},
+        addToQuery = function (attributeName) {
+          var values = selectedValues($scope.filters[attributeName]);
+          if (values.length > 0) {
+            queryOptions[attributeName] = values;
+          }
+        };
+
+    addToQuery('data_categories');
+    addToQuery('formats');
+    addToQuery('observation_types');
+    addToQuery('geographical_ranges');
+
+    console.log(queryOptions);
+
+    MapSearch.query(queryOptions).then(function(result) {
+      $scope.$broadcast('searchResults', result);
+    });
+  };
 
 }]);
 
--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -1,6 +1,10 @@
 class DatasetsController < ApplicationController
+  inject :data_categories, :data_formats, :observation_types, :geographical_ranges
+  helper_method :data_categories, :data_formats, :observation_types, :geographical_ranges
+
   # GET /datasets
   def index
+    @search = MapSearch.new(sort_by: 'creation_date')
     @decisions = Decision.where(:parent_id => nil).order("order_number ASC")
   end
 

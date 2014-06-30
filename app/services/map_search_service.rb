68e45bae2a14eb66dc1ccafe81d1cdbe192cdf83
Rafael Caricio <rafael.jacinto@gmail.com>
2014-06-30 16:25:26 +0200

    Return only one instance of each dataset

--- app/services/map_search_service.rb
+++ app/services/map_search_service.rb
@@ -7,6 +7,6 @@ class MapSearchService
     query_result = query_result.where("observation_type IN (?)", search.observation_types) if search.observation_types
     query_result = query_result.where("geographical_range IN (?)", search.geographical_ranges) if search.geographical_ranges
     query_result = query_result.joins(:data_categories).where("data_categories.id IN (?)", search.data_categories.map(&:to_i)) if search.data_categories
-    query_result
+    query_result.uniq
   end
 end

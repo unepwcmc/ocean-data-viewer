f912e078f42c2b26e84239f87c7a12336f3adb55
Rafael Caricio <rafael.jacinto@gmail.com>
2014-07-01 13:53:59 +0200

    Use names of categories in the multiselector element

--- app/assets/javascripts/angular/directives/multi-selector.js
+++ app/assets/javascripts/angular/directives/multi-selector.js
@@ -5,7 +5,7 @@ module.directive('multiSelector', ['$compile', '$rootScope', function($compile, 
     restrict: 'E',
     template: '<div class="multi-selector-widget">\
                  <div ng-click="visible=!visible" ng-class="[\'filter-select\', selected.length >=1 && \'active\' ]">\
-                    <span ng-bind="titleLabel"></span>\
+                    <span class="title-label" ng-bind="titleLabel"></span>\
                     <i ng-class="visible ? \'icon-angle-up\' : \'icon-angle-down\'"></i>\
                  </div>\
                   <div ng-class="[\'options\', visible && \'visible\']">\
@@ -21,15 +21,38 @@ module.directive('multiSelector', ['$compile', '$rootScope', function($compile, 
     link: function($scope, $element) {
       $scope.selected = [];
       $scope.visible = false;
+      var numberMaxOfCharecters = 26;
+
+      var getName = function(categoryKey) {
+        return $element.find('input[id="' + categoryKey + '-id"] + label').text();
+      };
+
       $scope.$watchCollection('model', function(newValue) {
-          $scope.selected = selectedValues(newValue);
-          if ($scope.selected.length == 1) {
-              $scope.titleLabel = "1 category";
-          } else if ($scope.selected.length > 1) {
-              $scope.titleLabel = $scope.selected.length + " categories";
-          } else {
-              $scope.titleLabel = "Any";
-          };
+        $scope.selected = selectedValues(newValue);
+
+        if ($scope.selected.length == 1) {
+          $scope.titleLabel = "1 category";
+
+          // use the name in case it's less than 26 characters
+          var categoryName = getName($scope.selected[0]);
+          if (categoryName.length <= numberMaxOfCharecters) {
+            $scope.titleLabel = categoryName;
+          }
+        } else if ($scope.selected.length > 1) {
+          var categoriesNames = [];
+          $scope.titleLabel = $scope.selected.length + " categories";
+
+          for (var i=0;i<$scope.selected.length;i++) {
+            categoriesNames.push(getName($scope.selected[i]));
+          }
+          categoriesNames = categoriesNames.join(', ');
+
+          if (categoriesNames.length <= numberMaxOfCharecters) {
+            $scope.titleLabel = categoriesNames;
+          }
+        } else {
+          $scope.titleLabel = "Any";
+        };
       });
     }
   };

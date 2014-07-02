98a8b9ec76cb79c51453dff8e96516996ff8bf47
Julia Jacobs <julia@llp.pl>
2014-07-02 14:09:03 +0200

    Add tooltip placeholders for data categories

--- app/serializers/dataset_serializer.rb
+++ app/serializers/dataset_serializer.rb
@@ -1,7 +1,7 @@
 class DatasetSerializer < ActiveModel::Serializer
   attributes :id, :title, :wms_server, :wms_name, :wfs, :order_number,
       :has_legend, :format, :observation_type, :geographical_range,
-      :creation_date, :url, :categories_names, :time_range_start,
+      :creation_date, :url, :data_categories, :time_range_start,
       :time_range_end, :time_range, :data_provider, :shp_download,
       :factsheet_url, :arcgis_link, :styles, :view_params
 
@@ -11,10 +11,6 @@ class DatasetSerializer < ActiveModel::Serializer
 
   def creation_date
     object.creation_date.strftime("%Y") if object.creation_date
-  end
-
-  def categories_names
-    object.data_categories.map(&:name).join(', ')
   end
 
   def time_range

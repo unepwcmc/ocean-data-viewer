ad8989f67ec538dec2c0fad05ac48fd89a5fe3c4
maxime <maxycle@hotmail.com>
2022-09-15 15:10:05 +0100

    fix: remove branch setup

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,4 @@
 set :stage, :staging
-set :branch, 'update-about-page'
 
 server "web-staging.linode.unep-wcmc.org", user: 'wcmc', roles: %w{app web db}
 server "db-staging.linode.unep-wcmc.org", user: 'wcmc', roles: %w{bkp}, no_release: true
